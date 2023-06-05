115249a87b9063aa47fcc8f03443b9382fba20d8
nedagosa-cod <nestorcode4004@gmail.com>
2023-06-05 12:42:28 -0500

    test

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import {PORT} from './config.js'
 import indexRoutes from './routes/index.routes.js'
+import testRoutes from './routes/test.routes.js'
 
 const app = express()
 
 app.use(indexRoutes)
+app.use(testRoutes)
 
 app.listen(PORT)
 console.log('server is running on server ' + PORT)
