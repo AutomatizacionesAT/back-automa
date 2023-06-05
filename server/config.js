e6291fafe71e0fba1b91d79202f85c8d67ed5454
nedagosa-cod <nestorcode4004@gmail.com>
2023-06-05 13:38:29 -0500

    add variables de entorno

--- server/config.js
+++ server/config.js
@@ -1,5 +1,12 @@
+// export const PORT = process.env.PORT || 3000
+// export const DB_PORT = process.env.PORT || 3000
+// export const DB_HOST = process.env.HOST || 'localhost'
+// export const DB_USER = process.env.USER || 'root'
+// export const DB_PASS = process.env.PASS || ''
+
 export const PORT = process.env.PORT || 3000
-export const DB_PORT = process.env.PORT || 7605
-export const DB_HOST = process.env.HOST || 'containers-us-west-126.railway.app'
-export const DB_USER = process.env.USER || 'root'
-export const DB_PASS = process.env.PASS || 'ZlWj0XiFoUQhaV5TEYNW'
+export const DB_PORT = process.env.DB_PORT || 3306
+export const DB_HOST = process.env.DB_HOST || 'localhost'
+export const DB_USER = process.env.DB_USER || 'root'
+export const DB_PASS = process.env.DB_PASS || ''
+export const DB_NAME = process.env.DB_NAME || 'autodb'
